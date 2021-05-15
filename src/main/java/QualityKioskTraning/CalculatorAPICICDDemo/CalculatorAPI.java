26c9f8cffb1c6b0b5ca9f0019beda3c38a32c420
Syed <afzalalisyed836@gmail.com>
2021-05-15 11:14:47 +0530

    change var

--- src/main/java/QualityKioskTraning/CalculatorAPICICDDemo/CalculatorAPI.java
+++ src/main/java/QualityKioskTraning/CalculatorAPICICDDemo/CalculatorAPI.java
@@ -2,7 +2,7 @@ package QualityKioskTraning.CalculatorAPICICDDemo;
 
 public class CalculatorAPI {
 	public int Addition(int num1,int num2) {
-		int Res= num1+num2;
-		return Res;
+		int R= num1+num2;
+		return R;
 		}
 }
